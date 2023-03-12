6218788093363cc8a523e0bb594130fede5a9e5d
Fulya <fulya.ertay@gmail.com>
2023-03-12 15:31:17 +0300

    added components

--- 1-first-react-site/index.js
+++ 1-first-react-site/index.js
@@ -1,14 +1,4 @@
 
-/**
-Challenge: 
-
-- Add an `ul` inside the Header's `nav` and create
-  the following `li`s: "Pricing", "About", & "Contact"
-- Using flexbox, line up the nav items horizontally, and
-  put them inline with the React logo.
-- Change the image styling to happen in CSS instead of in-line
-  For practice, add a new class to the image in order to style it
-*/
 
 function Header() {
     return (
