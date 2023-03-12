25879e8599ea5b9b66b99e8274ab4b09c64eed02
Fulya <fulya.ertay@gmail.com>
2023-03-12 17:03:09 +0300

    added main component

--- 1-first-react-site/src/components/Main.js
+++ 1-first-react-site/src/components/Main.js
@@ -1,5 +1,26 @@
+/**
+Challenge: Build the main section!
+
+Skip 2 aspects of the design for now:
+1. The colored bullets in the list
+2. The larger React logo on the side
+
+Those will be separate challenges coming up.
+*/
+
 import React from "react"
 
 export default function Main() {
-    return <h1>Main component</h1>
+    return (
+        <main>
+            <h1 className="main--title">Fun facts about React</h1>
+            <ul className="main--facts">
+                <li>Was first released in 2013</li>
+                <li>Was originally created by Jordan Walke</li>
+                <li>Has well over 100K stars on GitHub</li>
+                <li>Is maintained by Facebook</li>
+                <li>Powers thousands of enterprise apps, including mobile apps</li>
+            </ul>
+        </main>
+    )
 }
