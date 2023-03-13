05a329aef5d1d6318275e98bc44dafdea8fd91bf
Fulya <fulya.ertay@gmail.com>
2023-03-13 18:13:20 +0300

    added footer

--- 2-digital-business-card/src/components/Footer.js
+++ 2-digital-business-card/src/components/Footer.js
@@ -1,8 +1,16 @@
 import React from "react";
+import TwitterIcon from "../images/Twitter Icon.png"
+import FacebookIcon from "../images/Facebook Icon.png"
+import InstagramIcon from "../images/Instagram Icon.png"
+import GithubIcon from "../images/GitHub Icon.png"
 
 export default function Footer() {
   return <div className="footer">
+    <img src={TwitterIcon} alt="twitter"/>
+    <img src={FacebookIcon} alt="facebook"/>
+    <img src={InstagramIcon} alt="instagram"/>
+    <img src={GithubIcon} alt="Github"/>
 
-    
+
   </div>;
 }
