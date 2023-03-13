5531d6a73435bafb0e019afe1b536a22abd7584a
Fulya <fulya.ertay@gmail.com>
2023-03-13 17:15:40 +0300

    added job title

--- 2-digital-business-card/src/components/Header.js
+++ 2-digital-business-card/src/components/Header.js
@@ -4,6 +4,9 @@ export default function Header(){
     return(
         <div className="header">
             <img src={avatar} alt="avatar" className="avatar"></img>
+            <h3 className="name">Fulya Ertay</h3>
+            <p className="job">Frontend Developer</p>
+
         </div>
     )
 }
