7b65a803facd6b7808bf199457e9c28b151f218c
Fulya <fulya.ertay@gmail.com>
2023-03-16 20:47:19 +0300

    added data.js

--- 4-travel-journal-app/src/App.js
+++ 4-travel-journal-app/src/App.js
@@ -2,11 +2,15 @@
 import './App.css';
 import Header from "./components/Header"
 import Places from "./components/Places"
+import data from "./data"
 function App() {
+  const places=data.map(item=>{
+    return <Places item={item}></Places>
+  })
   return (
     <div className="App">
       <Header/>
- 
+      {places}
     
     </div>
   );
