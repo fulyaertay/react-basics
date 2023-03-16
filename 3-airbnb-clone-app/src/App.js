2c72220a2d85d1be2bf5dc948ef5bd59c1ba73ff
Fulya <fulya.ertay@gmail.com>
2023-03-16 16:34:58 +0300

    added card section

--- 3-airbnb-clone-app/src/App.js
+++ 3-airbnb-clone-app/src/App.js
@@ -14,7 +14,12 @@ export default function App() {
         <div>
             <Navbar />
             <Hero/>
-            <Card/>
+            <Card img="../images/katie-zaferes.png"
+                rating="5.0"
+                reviewCount={6}
+                country="USA"
+                title="Life Lessons with Katie Zaferes"
+                price={136}/>
         </div>
     )
 }
--- 3-airbnb-clone-app/src/components/Card.js
+++ 3-airbnb-clone-app/src/components/Card.js
@@ -19,18 +19,18 @@ Notes:
   this data into the component.
 */
 
-export default function Card() {
+export default function Card(props) {
     return (
         <div className="card">
-            <img src={cardImage} className="card--image" />
+            <img src={`../images/${props.img}`} className="card--image" />
             <div className="card--stats">
                 <img src="../images/star.png" className="card--star" />
-                <span>5.0</span>
-                <span className="gray">(6) • </span>
-                <span className="gray">USA</span>
+                <span>{props.rating}</span>
+                <span className="gray">({props.reviewCount}) • </span>
+                <span className="gray">{props.country}</span>
             </div>
-            <p>Life Lessons with Katie Zaferes</p>
-            <p><span className="bold">From $136</span> / person</p>
+            <p>{props.title}</p>
+            <p><span className="bold">From ${props.price}</span> / person</p>
         </div>
     )
 }
