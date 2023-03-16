79351559311db400baa8e8a2c8fe48406a1fac37
Fulya <fulya.ertay@gmail.com>
2023-03-16 13:41:43 +0300

    fist-init

---
+++ 3-airbnb-clone-app/src/App.js
@@ -0,0 +1,14 @@
+import React from "react"
+import Navbar from "./components/Navbar"
+/*
+Challenge: Build the Navbar component.
+Check the Figma file for the design specifics.
+*/
+
+export default function App() {
+    return (
+        <div>
+            <Navbar />
+        </div>
+    )
+}
