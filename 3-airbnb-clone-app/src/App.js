0e8c9f23f582af6cfae4a5fa7090d1f585450202
Fulya <fulya.ertay@gmail.com>
2023-03-16 19:01:39 +0300

    formatted documents

--- 3-airbnb-clone-app/src/App.js
+++ 3-airbnb-clone-app/src/App.js
@@ -1,30 +1,20 @@
-import React from "react"
-import Navbar from "./components/Navbar"
-import Hero from "./components/Hero"
-import Card from "./components/Card"
-import './App.css';
-import data from "./data"
-
+import React from "react";
+import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
+import Card from "./components/Card";
+import "./App.css";
+import data from "./data";
 
 export default function App() {
+  const cards = data.map((item) => {
+    return <Card key={item.id} item={item} />;
+  });
 
-      const cards = data.map(item => {
-        return (
-          <Card
-          key={item.id}
-          item={item}
-         
-      />
-        )
-    })        
-    
-    return (
-      <div>
+  return (
+    <div>
       <Navbar />
       <Hero />
-      <section className="cards-list">
-          {cards}
-      </section>
-  </div>
-    )
+      <section className="cards-list">{cards}</section>
+    </div>
+  );
 }
--- 3-airbnb-clone-app/src/components/Card.js
+++ 3-airbnb-clone-app/src/components/Card.js
@@ -1,5 +1,4 @@
-import React from "react"
-
+import React from "react";
 
 /*
 Challenge: Build the Card component
@@ -21,25 +20,31 @@ Notes:
 */
 
 export default function Card(props) {
-    let badgeText
-    if (props.item.openSpots === 0) {
-        badgeText = "SOLD OUT"
-    } else if (props.item.location === "Online") {
-        badgeText = "ONLINE"
-    }
-    
-    return (
-        <div className="card">
-            {badgeText && <div className="card--badge">{badgeText}</div>}
-            <img src={props.item.coverImg} className="card--image" alt="cover-image" />
-            <div className="card--stats">
-                <img src="../images/star.png" className="card--star" alt="icon" />
-                <span>{props.item.stats.rating}</span>
-                <span className="gray">({props.item.stats.reviewCount}) • </span>
-                <span className="gray">{props.item.location}</span>
-            </div>
-            <p>{props.item.title}</p>
-            <p><span className="bold">From ${props.item.price}</span> / person</p>
-        </div>
-    )
+  let badgeText;
+  if (props.item.openSpots === 0) {
+    badgeText = "SOLD OUT";
+  } else if (props.item.location === "Online") {
+    badgeText = "ONLINE";
+  }
+
+  return (
+    <div className="card">
+      {badgeText && <div className="card--badge">{badgeText}</div>}
+      <img
+        src={props.item.coverImg}
+        className="card--image"
+        alt="cover-image"
+      />
+      <div className="card--stats">
+        <img src="../images/star.png" className="card--star" alt="icon" />
+        <span>{props.item.stats.rating}</span>
+        <span className="gray">({props.item.stats.reviewCount}) • </span>
+        <span className="gray">{props.item.location}</span>
+      </div>
+      <p>{props.item.title}</p>
+      <p>
+        <span className="bold">From ${props.item.price}</span> / person
+      </p>
+    </div>
+  );
 }
