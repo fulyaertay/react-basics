c6dcfeb4042924b3516fccb0eb290d4496c6db8f
Fulya <fulya.ertay@gmail.com>
2023-03-16 18:16:56 +0300

    added key prop

--- 3-airbnb-clone-app/src/App.js
+++ 3-airbnb-clone-app/src/App.js
@@ -11,6 +11,7 @@ export default function App() {
       const cards = data.map(item => {
         return (
             <Card 
+                key={item.id}
                 img={item.coverImg}
                 rating={item.stats.rating}
                 reviewCount={item.stats.reviewCount}
