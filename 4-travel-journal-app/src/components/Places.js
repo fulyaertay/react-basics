594031dcd9c849c7fea02622b418dc9bd4604a9f
Fulya <fulya.ertay@gmail.com>
2023-03-16 21:18:58 +0300

    added location info on each card

--- 4-travel-journal-app/src/components/Places.js
+++ 4-travel-journal-app/src/components/Places.js
@@ -1,15 +1,22 @@
 import React from "react";
 
-export default function Places(props){
-    return(
-        <div className="card--list">
-            <img src={props.img} className="travel--image"></img>
-            <div className="place--content">
-
-
-            </div>
-
+export default function Places(props) {
+  return (
+    <div className="card--list">
+      <img src={props.item.img} className="travel--image"></img>
+      <div className="place--content">
+        <div className="places--content-header">
+          <img src="../images/location.png" className="location--logo"></img>
+          <p>{props.item.location}</p>
+          <p className="view--maps">View on Google Maps</p>
         </div>
-    )
-
+        <div>
+        <p>{props.item.location}</p>
+        </div>
+        <div>
+        <p>{props.item.location}</p>
+        </div>
+      </div>
+    </div>
+  );
 }
