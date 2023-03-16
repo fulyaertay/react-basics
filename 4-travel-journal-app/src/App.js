8433f3dbfb78b5d6c93582a51cd79109f208c415
Fulya <fulya.ertay@gmail.com>
2023-03-16 20:29:57 +0300

    added data.js

--- 4-travel-journal-app/src/App.js
+++ 4-travel-journal-app/src/App.js
@@ -1,6 +1,7 @@
 
 import './App.css';
-
+import Header from "./components/Header"
+import Places from "./components/Places"
 function App() {
   return (
     <div className="App">
---
+++ 4-travel-journal-app/src/data.js
@@ -0,0 +1,30 @@
+export default[
+    {
+        id:1,
+        img:"../images/mount-fuji.png",
+        title: "Mount Fuji",
+        location: "JAPAN",
+        date:"12 Jan, 2021 - 24 Jan, 2021",
+        description: "Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists."
+
+    },
+    {
+        id:2,
+        img:"../images/sydney.png",
+        title: "Sydney Opera House",
+        location: "AUSTRALIA",
+        date:"27 May, 2021 - 8 Jun, 2021",
+        description: "The Sydney Opera House is a multi-venue performing arts centre in Sydney. Located on the banks of the Sydney Harbour, it is often regarded as one of the 20th century's most famous and distinctive buildings."
+
+    },
+    {
+        id:3,
+        img:"../images/norway.png",
+        title: "Geirangerfjord",
+        location: "NORWAY",
+        date:"01 Oct, 2021 - 18 Nov, 2021",
+        description: "The Geiranger Fjord is a fjord in the Sunnmøre region of Møre og Romsdal county, Norway. It is located entirely in the Stranda Municipality."
+
+    }
+
+]
