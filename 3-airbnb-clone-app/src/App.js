1b22f2ed90dc59d6ae7593c61021fd3a8ea5a192
Fulya <fulya.ertay@gmail.com>
2023-03-16 18:32:51 +0300

    added badgeText

--- 3-airbnb-clone-app/src/App.js
+++ 3-airbnb-clone-app/src/App.js
@@ -10,15 +10,16 @@ export default function App() {
 
       const cards = data.map(item => {
         return (
-            <Card 
-                key={item.id}
-                img={item.coverImg}
-                rating={item.stats.rating}
-                reviewCount={item.stats.reviewCount}
-                location={item.location}
-                title={item.title}
-                price={item.price}
-            />
+          <Card
+          key={item.id}
+          img={item.coverImg}
+          rating={item.stats.rating}
+          reviewCount={item.stats.reviewCount}
+          location={item.location}
+          title={item.title}
+          price={item.price}
+          openSpots={item.openSpots}
+      />
         )
     })        
     
