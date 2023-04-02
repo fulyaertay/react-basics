b8f6b639cbdb98f7173b0fc65279bbd259febba2
Fulya <fulya.ertay@gmail.com>
2023-04-02 20:26:18 +0300

    change dice to objects

--- 7-tenzies-app/App.js
+++ 7-tenzies-app/App.js
@@ -1,14 +1,27 @@
 import React from "react"
 import Die from "./Die"
+import {nanoid} from "nanoid"
 
 export default function App() {
+/**
+ * Challenge: Update the array of numbers in state to be
+ * an array of objects instead. Each object should look like:
+ * { value: <random number>, isHeld: false }
+ * 
+ * Making this change will break parts of our code, so make
+ * sure to update things so we're back to a working state
+ */
 
     const [dice, setDice] = React.useState(allNewDice())
     
     function allNewDice() {
         const newDice = []
         for (let i = 0; i < 10; i++) {
-            newDice.push(Math.ceil(Math.random() * 6))
+            newDice.push({
+                value: Math.ceil(Math.random() * 6), 
+                isHeld: false,
+                id: nanoid()
+            })
         }
         return newDice
     }
@@ -17,7 +30,9 @@ export default function App() {
         setDice(allNewDice())
     }
     
-    const diceElements = dice.map(die => <Die value={die} />)
+    const diceElements = dice.map(die => (
+        <Die key={die.id} value={die.value} />
+    ))
     
     return (
         <main>
