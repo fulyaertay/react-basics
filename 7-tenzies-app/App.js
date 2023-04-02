973f85750e767fcb75d426c99cf649529760b309
Fulya <fulya.ertay@gmail.com>
2023-04-02 23:03:21 +0300

    added react-confetti

--- 7-tenzies-app/App.js
+++ 7-tenzies-app/App.js
@@ -1,6 +1,7 @@
 import React from "react"
 import Die from "./Die"
 import {nanoid} from "nanoid"
+import Confetti from "react-confetti"
 
 export default function App() {
 
@@ -17,12 +18,13 @@ export default function App() {
         }
     }, [dice])
 /**
- * Challenge: Check the dice array for these winning conditions:
- * 1. All dice are held, and
- * 2. all dice have the same value
+ * Challenge: Tie off loose ends!
+ * 1. If tenzies is true, Change the button text to "New Game"
+ * 2. If tenzies is true, use the "react-confetti" package to
+ *    render the <Confetti /> component 🎉
  * 
- * If both conditions are true, set `tenzies` to true and log
- * "You won!" to the console
+ *    Hint: don't worry about the `height` and `width` props
+ *    it mentions in the documentation.
  */
 
     function generateNewDie() {
@@ -69,12 +71,18 @@ export default function App() {
     
     return (
         <main>
+            {tenzies && <Confetti />}
             <h1 className="title">Tenzies</h1>
             <p className="instructions">Roll until all dice are the same. Click each die to freeze it at its current value between rolls.</p>
             <div className="dice-container">
                 {diceElements}
             </div>
-            <button className="roll-dice" onClick={rollDice}>Roll</button>
+            <button 
+                className="roll-dice" 
+                onClick={rollDice}
+            >
+                {tenzies ? "New Game" : "Roll"}
+            </button>
         </main>
     )
 }
