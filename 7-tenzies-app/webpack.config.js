8b140f59068be094d3636b66c7037ca9c282b794
Fulya <fulya.ertay@gmail.com>
2023-04-02 14:07:38 +0300

    rest-spread added

--- 7-tenzies-app/webpack.config.js
+++ 7-tenzies-app/webpack.config.js
@@ -11,7 +11,8 @@ module.exports = {
             "presets": [
               "babel-preset-env",
               "babel-preset-react"
-            ]
+            ],
+            "plugins": ["babel-plugin-transform-object-rest-spread"]
           }
         },
         "exclude": /node_modules/,
