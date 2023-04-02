5cf55f62c3e3c1b08dcec7ddfd724ee1f80e0b25
Fulya <fulya.ertay@gmail.com>
2023-04-02 21:11:41 +0300

    styling held dice

--- 7-tenzies-app/App.js
+++ 7-tenzies-app/App.js
@@ -4,12 +4,12 @@ import {nanoid} from "nanoid"
 
 export default function App() {
 /**
- * Challenge: Update the array of numbers in state to be
- * an array of objects instead. Each object should look like:
- * { value: <random number>, isHeld: false }
+ * Challenge: Add conditional styling to the Die component
+ * so that if it's held (isHeld === true), its background color
+ * changes to a light green (#59E391)
  * 
- * Making this change will break parts of our code, so make
- * sure to update things so we're back to a working state
+ * Remember: currently the Die component has no way of knowing
+ * if it's "held" or not.
  */
 
     const [dice, setDice] = React.useState(allNewDice())
@@ -31,7 +31,7 @@ export default function App() {
     }
     
     const diceElements = dice.map(die => (
-        <Die key={die.id} value={die.value} />
+        <Die key={die.id} value={die.value} isHeld={die.isHeld} />
     ))
     
     return (
