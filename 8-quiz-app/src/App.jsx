eb856b639a8066da4b88f098d21043d747593812
Fulya <fulya.ertay@gmail.com>
2023-04-06 16:29:11 +0300

    main page completed

--- 8-quiz-app/src/App.jsx
+++ 8-quiz-app/src/App.jsx
@@ -1,18 +1,14 @@
 import { useState } from 'react'
-
+import MainPage from './components/main'
 import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <div className="container">
-      <h1 className='mainText'>Quizzical</h1>
-      <h4 className='mainText'>Let's Play!</h4>
-      <button className='startQuizBtn'>Start Quiz</button>
-
-     
-    </div>
+    <>
+      <MainPage></MainPage>
+    </>
   )
 }
 
