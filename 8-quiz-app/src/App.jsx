94dfbe80cb142205a5c345a032517d92b07f1f13
Fulya <fulya.ertay@gmail.com>
2023-04-06 16:24:29 +0300

    main page completed

--- 8-quiz-app/src/App.jsx
+++ 8-quiz-app/src/App.jsx
@@ -6,7 +6,11 @@ function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <div className="App">
+    <div className="container">
+      <h1 className='mainText'>Quizzical</h1>
+      <h4 className='mainText'>Some description if needed</h4>
+      <button className='startQuizBtn'>Start Quiz</button>
+
      
     </div>
   )
