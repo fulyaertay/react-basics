1ba940bff5d1e6351319eeb5e88acd3a0764812e
Fulya <fulya.ertay@gmail.com>
2023-04-06 19:44:38 +0300

    answers addded

--- 8-quiz-app/src/components/QuizPage.jsx
+++ 8-quiz-app/src/components/QuizPage.jsx
@@ -1,14 +1,18 @@
 import React from "react"
 
 export default function QuizPage(props) {
-
+  console.log(props.answers)
   return (
     
 
       <div className="questions">
         <h3 className='mainText'>{props.question}</h3>
         <div className='choices'>
-          <button className="choiceBtn">{props.answers}</button>
+          {props.answers.map(answer=>{
+            return <button className="choiceBtn">{answer}</button>
+
+          })}
+         
         </div>
       </div>
       
