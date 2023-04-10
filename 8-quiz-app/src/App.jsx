4dd7a8f78fa78ffbaf7dbf2f5b111037f82a5bcb
Fulya <fulya.ertay@gmail.com>
2023-04-10 12:40:52 +0300

    added userAnswers

--- 8-quiz-app/src/App.jsx
+++ 8-quiz-app/src/App.jsx
@@ -8,7 +8,10 @@ function App() {
   const [questions, setQuestions]=useState([])
   const [clickedAnswer,setClickedAnswer]=useState(false)
   const [start,setStart]=useState(true)
-  
+  const [userAnswers, setUserAnswers] = useState({});
+  const [score, setScore] = useState(0);
+
+  let incorrectAnswers = []
   useEffect(()=>{
     fetch("https://opentdb.com/api.php?amount=5")
     .then(res=>res.json())
@@ -27,7 +30,9 @@ function App() {
     <Questions 
         key={nanoid()} 
         question={question.question} 
-        answers={ [question.incorrect_answers.map(incorrect_answer=>{return incorrect_answer}),
+        answers={ [question.incorrect_answers[0] ? question.incorrect_answers[0]:undefined,
+          question.incorrect_answers[1] ? question.incorrect_answers[1]:undefined,
+          question.incorrect_answers[2] ? question.incorrect_answers[2]:undefined,
           question.correct_answer]}
           toggleClick={toggleClick}
           isClicked={clickedAnswer}
