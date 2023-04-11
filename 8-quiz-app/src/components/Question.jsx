261793554f7fb5b74bdd98a9467e2aa8aec0393b
Fulya <fulya.ertay@gmail.com>
2023-04-11 13:55:19 +0300

    removed answes state

--- 8-quiz-app/src/components/Question.jsx
+++ 8-quiz-app/src/components/Question.jsx
@@ -12,6 +12,7 @@ export default function Question(props) {
         <h3 className='mainText'>{props.question}</h3>
         <div className='choices'>
           {props.answers.map(answer=>{
+            
             if (answer!=undefined)
             return <button id={props.key} className="choiceBtn" style={styles} onClick={props.toggleClick}>{answer}</button>
 
