ba324fe9f1c914631a0f4835862d43178ae1b884
Fulya <fulya.ertay@gmail.com>
2023-04-13 21:31:28 +0300

    deleted unneccesary folders

--- 8-quiz-app/vite.config.js
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react(), ["babel-plugin-transform-object-rest-spread"]],
-  
-})
