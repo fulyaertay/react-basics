343656de4cb4d4ea0abfaefd3fc619c176751b36
Fulya <fulya.ertay@gmail.com>
2023-05-05 11:33:41 +0300

    build product

--- 6-notes-app/App.js
+++ 6-notes-app/App.js
@@ -1,7 +1,6 @@
 import React from "react"
 import Sidebar from "./components/Sidebar"
 import Editor from "./components/Editor"
-import { data } from "./data"
 import Split from "react-split"
 import {nanoid} from "nanoid"
 
